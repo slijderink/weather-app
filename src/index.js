6e146342205d9ec4b557e6fb34f8a0d97a757286
slijderink <72390050+slijderink@users.noreply.github.com>
2020-11-09 12:36:01 +0100

    Added celsius and fahrenheit conversion

--- src/index.js
+++ src/index.js
@@ -46,7 +46,6 @@ function search(city) {
   axios.get(apiUrl).then(displayWeather);
 }
 
-
 function displayWeather(response) {
   console.log(response.data.name);
   document.querySelector(`#city`).innerHTML = response.data.name;
@@ -129,14 +128,18 @@ weatherBangkok.addEventListener("click", showWeatherBangkok);
 
 function displayFahrenheitTemp(event){
   event.preventDefault();
-  let fahrenheitTemperature=(celsiusTemperature*9)/5+32;
   let temperatureElement=document.querySelector(`#temperature`)
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature=(celsiusTemperature*9)/5+32;
   temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
 }
 
 function displayCelsiusTemp(event){
   event.preventDefault();
   let temperatureElement=document.querySelector(`#temperature`)
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   temperatureElement.innerHTML=Math.round(celsiusTemperature);
 }
 let fahrenheitLink=document.querySelector(`#fahrenheit-link`)
